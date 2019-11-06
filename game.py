c511a8a9060a719221024fde0610bfe733fbcb55
unknown <frankfrankfrankyeyeye@gmail.com>
2019-11-05 21:55:56 -0500

    Added cache busting headers

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ from os.path import isfile, join
 from random import shuffle
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
+import webbrowser
 
 HOST_NAME = '0.0.0.0'
 PORT_NUMBER = 80
@@ -15,6 +16,9 @@ class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         global counter
         self.send_response(200)
+        self.send_header('Cache-Control', 'no-cache, no-store, must-revalidate')
+        self.send_header('Pragma', 'no-cache')
+        self.send_header('Expires', '0')
         if counter == len(onlyfiles):
             self.send_header('Content-type', 'text/html')
             with open('./html/no-images.html', 'rb') as f:
@@ -32,6 +36,7 @@ if __name__ == '__main__':
     httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
     print(time.asctime(), 'Server Starts - %s:%s' % (HOST_NAME, PORT_NUMBER))
     try:
+        webbrowser.open('http://localhost:80')
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
